2abe8d82da3888a5399a3c03be900275cf22c8c1
Chang Hyun Park <heartinpiece@gmail.com>
2022-01-11 12:04:28 +0100

    Latest module updates

--- UPPMAX_modules.sh
+++ UPPMAX_modules.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-module load gcc/6.4.0
+#module load gcc/6.4.0
 
 module load ABINIT/8.10.3
 
 module load Qt5/5.13.1-GCCcore-8.3.0
 
-module load fosscuda/2018b
+#module load fosscuda/2018b
+#module load CUDA/10.1.243-GCC-8.3.0
+module load CUDA/11.1.1-GCC-10.2.0
