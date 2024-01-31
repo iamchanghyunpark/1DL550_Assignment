f95f38d00d7e885e1acb7ff62bcb857910412098
Chang Hyun Park <heartinpiece@gmail.com>
2024-01-31 15:42:29 +0100

    Updates UPPMAX_modules.sh
    Fixes some dependency that UPPMAX suddenly introduced.

--- UPPMAX_modules.sh
+++ UPPMAX_modules.sh
@@ -6,6 +6,8 @@ module load ABINIT/8.10.3
 
 module load Qt5/5.13.1-GCCcore-8.3.0
 
+module load python_ML_packages/3.9.5-gpu
+
 #module load fosscuda/2018b
 #module load CUDA/10.1.243-GCC-8.3.0
 module load CUDA/11.1.1-GCC-10.2.0
